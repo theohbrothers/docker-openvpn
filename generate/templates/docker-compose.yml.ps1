3c4242bb9b72833fdccfc24e4881f400e3a2eaf2
Leo <leonardjonathanoh@live.com>
2020-03-24 07:56:56 +0000

    Fix indentation in docker-compose.yml

--- generate/templates/docker-compose.yml.ps1
+++ generate/templates/docker-compose.yml.ps1
@@ -5,10 +5,10 @@ services:
     container_name: openvpn
     image: theohbrothers/docker-openvpn:$( $VARIANT['tag'] )
     ports:
-     - "1194:1194/udp"
+      - "1194:1194/udp"
     cap_add:
       - NET_ADMIN
     restart: unless-stopped
     volumes:
-     - ./openvpn/server.conf:/etc/openvpn/server.conf
+      - ./openvpn/server.conf:/etc/openvpn/server.conf
 "@
