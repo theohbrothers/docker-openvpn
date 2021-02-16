e212acd2d010f53467e9232a2e6c621e946c333b
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-02-16 15:09:24 +0000

    Docs: Replace microbadger image size and image layers badges with shields.io docker image size badge in readme

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -3,8 +3,7 @@
 
 [![github-actions](https://github.com/theohbrothers/docker-openvpn/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-openvpn/actions)
 [![github-tag](https://img.shields.io/github/tag/theohbrothers/docker-openvpn)](https://github.com/theohbrothers/docker-openvpn/releases/)
-[![docker-image-size](https://img.shields.io/microbadger/image-size/theohbrothers/docker-openvpn/latest)](https://hub.docker.com/r/theohbrothers/docker-openvpn)
-[![docker-image-layers](https://img.shields.io/microbadger/layers/theohbrothers/docker-openvpn/latest)](https://hub.docker.com/r/theohbrothers/docker-openvpn)
+[![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-openvpn/latest)](https://hub.docker.com/r/theohbrothers/docker-openvpn)
 
 Dockerized ``openvpn``.
 
