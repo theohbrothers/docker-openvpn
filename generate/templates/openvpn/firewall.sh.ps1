a58c301c4486de55bbd4d5710b41777a75653bb1
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2022-12-23 09:28:26 +0000

    Docs (firewall.sh): Improve documentation in `firewall.sh

--- generate/templates/openvpn/firewall.sh.ps1
+++ generate/templates/openvpn/firewall.sh.ps1
@@ -1,9 +1,10 @@
 @'
 #!/bin/sh
 
-# This iptables script is useful for openpvn in server mode. Not so much for client mode.
+# This iptables script to controlling traffic in the openvpn tunnel.
+# In this example, clients can only perform DNS, HTTP and HTTPS requests to the world.
 
-set -eo pipefail
+set -eu -o pipefail
 
 # Drop everything by default from tunnel to world
 iptables -P FORWARD DROP
