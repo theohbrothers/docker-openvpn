4ab486e779436102d48cacf13beb1f90df91dc1e
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-27 09:13:38 +0000

    Enhancement: Test `openvpn` in Dockerfile

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -1,7 +1,9 @@
 @"
 FROM $( $VARIANT['_metadata']['distro'] ):$( $VARIANT['_metadata']['distro_version'] )
 
-RUN apk add --no-cache $( $VARIANT['_metadata']['package'] )$( if ([version]$VARIANT['_metadata']['distro_version'] -le [version]'3.6') { '>=' } else { '~=' } )$( $VARIANT['_metadata']['package_version'] ) iptables
+RUN set -eux; \
+    apk add --no-cache $( $VARIANT['_metadata']['package'] )$( if ([version]$VARIANT['_metadata']['distro_version'] -le [version]'3.6') { '>=' } else { '~=' } )$( $VARIANT['_metadata']['package_version'] ) iptables \
+    openvpn --version
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
