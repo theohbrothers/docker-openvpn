11db130856e49a1fdca5eca610b032d1e8914d45
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2024-04-27 09:59:39 +0000

    Enhancement: Add openvpn 2.6.10 variant

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -2,7 +2,7 @@
 FROM $( $VARIANT['_metadata']['distro'] ):$( $VARIANT['_metadata']['distro_version'] )
 
 RUN set -eux; \
-    apk add --no-cache $( $VARIANT['_metadata']['package'] )$( if ([version]$VARIANT['_metadata']['distro_version'] -le [version]'3.6') { '>=' } else { '~=' } )$( $VARIANT['_metadata']['package_version'] ) iptables; \
+    apk add --no-cache $( $VARIANT['_metadata']['package'] )$( if ($VARIANT['_metadata']['distro_version'] -ne 'edge' -and [version]$VARIANT['_metadata']['distro_version'] -le [version]'3.6') { '>=' } else { '~=' } )$( $VARIANT['_metadata']['package_version'] ) iptables; \
     # Workaround openvpn --version exiting with non-zero exit code on openvpn <= 2.4.x
     openvpn --version | grep -A100 -B100 $( $VARIANT['_metadata']['package_version'] )
 
